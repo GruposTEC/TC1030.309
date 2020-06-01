688f9a9b95e4f4fb20a1b5c69e4ad091255fa6bd
Antonio Mejia <avmejia@tec.mx>
2020-06-01 09:58:50 -0500

    ejericio 1 junio

--- Excepciones/excepcion.py
+++ Excepciones/excepcion.py
@@ -8,3 +8,26 @@ else:
 
 mal = direc + "prueba_1.txt"
 ok = direc + "prueba.txt"
+
+
+def metodo():
+    raise ZeroDivisionError("algo paso")
+
+
+try:
+    f = open(mal)
+    # metodo()
+    #var = novar
+except MemoryError as e:
+    print("error de memoria")
+    print(e)
+except NameError:
+    print("variable no reconocida")
+except Exception as e:
+    print(isinstance(e, ZeroDivisionError))
+    print(isinstance(e, NameError))
+    print("caida en la red")
+else:
+    print("No hubo excpeciones")
+finally:
+    print("siempre corro yo")
